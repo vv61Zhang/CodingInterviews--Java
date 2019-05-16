f7af89c9d4f9b85b97b0c36f78b70d313e3dd17e
Wei Zhang <sylvia031010@gmail.com>
2019-05-16 15:55:49 -0700

    Update 04_FindIntegerIn2dArray.java

--- src/04_FindIntegerIn2dArray.java
+++ src/04_FindIntegerIn2dArray.java
@@ -1,1 +1,15 @@
-
+public class Solution {
+    public boolean Find(int target, int [][] array) {
+        if(array == null ||  array.length == 0) return false;
+        int row = 0;
+        int col = array[0].length -1 ;
+        
+        while(row < array[0].length && col >=0 ){
+            int temp = array[row][col];
+            if(temp == target) return true;
+            else if (temp  > target) col -- ;
+            else row++;
+        }
+        return false;
+    }
+}
