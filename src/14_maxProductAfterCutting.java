c5554a5f5bf076ea040e8423c3ab0d45e7267ba3
Wei Zhang <sylvia031010@gmail.com>
2019-05-21 21:06:53 -0700

    Update 14_maxProductAfterCutting.java

--- src/14_maxProductAfterCutting.java
+++ src/14_maxProductAfterCutting.java
@@ -1,3 +1,5 @@
+
+//Solution 1 :动态规划
 public class Solutions {
 	public int maxProductAfterCutting(int n) {
 		//if(n<=1) return 0;
@@ -18,3 +20,22 @@ public class Solutions {
 		}	
 		return product[n];
 	}
+//Solution 2: 贪婪算法
+public static int maxProductWithGreedy(int len){
+		if(len<2)
+			return 0;
+		if(len==2)
+			return 1;
+		if(len==3)
+			return 2;
+		//啥也不管,先尽可能减去长度为3的段
+		int timeOfThree=len/3;
+		
+		//判断还剩下多少，再进行处理
+		if(len-timeOfThree*3==1)
+			timeOfThree-=1;
+		int timeOfTwo=(len-timeOfThree*3)/2;
+		
+		return (int) ((Math.pow(3, timeOfThree))*(Math.pow(2, timeOfTwo)));
+	
+	
